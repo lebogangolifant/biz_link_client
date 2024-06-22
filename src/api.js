f151f1903078e27ef4b670a57720ea808490d893
Lebogang <lebogangodeveloper@gmail.com>
2024-06-22 14:18:16 +0200

    Modify enviroment variable

--- src/api.js
+++ src/api.js
@@ -1,10 +1,8 @@
 // client/src/api.js
 import axios from 'axios';
 
-const baseURL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api';
-
 const api = axios.create({
-  baseURL: baseURL,
+  baseURL: process.env.REACT_APP_API_BASE_URL, // Use environment variable for baseURL
 });
 
 console.log('API Base URL:', baseURL);
