a3e2d7eacde5b520f94a25681af9f9fd0f0691c5
Lebogang <lebogangodeveloper@gmail.com>
2024-06-23 12:54:45 +0200

    Set API baseURL dynamically

--- src/api.js
+++ src/api.js
@@ -1,9 +1,13 @@
 // client/src/api.js
 import axios from 'axios';
 
+const baseURL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api';
+
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL, // Use environment variable for baseURL
+  baseURL: baseURL,
 });
+
+console.log('API Base URL:', baseURL);
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
